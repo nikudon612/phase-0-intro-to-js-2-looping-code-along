763695824609cd6022a852734d97ea98b250f056
Nick Bechtel <bechtel.nick@gmail.com>
2021-09-30 00:35:36 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const cards = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+console.log(writeCards(cards));
+
+function countDown() {
+  let i = "10";
+  while (i >= 0) {
+    console.log(i--);
+  }
+}
+countDown;
